49ebdce492c8c59a085343b7e023206d0af3dd95
Hoang Liem Nguyen <daniel.liemng@gmail.com>
2020-03-21 00:16:24 -0400

    Added Authentication

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': "Blog Post 1",
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': "Blog Post 2",
-        'content': 'Second Post Content',
-        'date_posted': 'August 30, 2019'
-    },
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': "Blog Post 1",
+#         'content': 'First Post Content',
+#         'date_posted': 'August 27, 2019'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': "Blog Post 2",
+#         'content': 'Second Post Content',
+#         'date_posted': 'August 30, 2019'
+#     },
+# ]
 
 
 def home(request):
+    posts = Post.objects.all()
+
     context = {"posts": posts}
+
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     context = {'title': 'About'}
+
     return render(request, 'blog/about.html', context)
