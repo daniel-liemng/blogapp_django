bee939b2b904be7e267d970c1d0874ad69f27e2c
Hoang Liem Nguyen <daniel.liemng@gmail.com>
2020-03-31 02:50:12 -0400

    Add class-based CRUD views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 # Create your views here.
@@ -19,10 +26,51 @@ class PostListView(ListView):
     # <app>/<model>_<viewtype>.html
     # blog/post_view.html
     template_name = 'blog/home.html'
-
     context_object_name = 'posts'
-
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Add the current user to be the author of the post to be created
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Add the current user to be the author of the post to be updated
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # Check if the current user is the author of the post to be updated
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    # Check if the current user is the author of the post to be deleted
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
